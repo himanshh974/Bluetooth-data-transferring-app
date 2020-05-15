7536aea9ce7470fc321ab6d37ae3003dd7efeba8
himanshh974 <60419105+himanshh974@users.noreply.github.com>
2020-05-15 21:59:53 +0530

    Add files via upload

---
+++ myappp/ledControl.java
@@ -0,0 +1,217 @@
+package com.mcuhq.myappp;
+
+
+import android.app.ProgressDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.IOException;
+import java.util.UUID;
+
+public class ledControl extends AppCompatActivity {
+
+    Button btn1, btn2, btn3, btn4, btn5, btn6,btnDis;
+    String address = null;
+    TextView lumn, readData;
+    private ProgressDialog progress;
+    BluetoothAdapter myBluetooth = null;
+    BluetoothSocket btSocket = null;
+    private boolean isBtConnected = false;
+    static final UUID myUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
+    Thread workerThread;
+    byte[] readBuffer;
+    int readBufferPosition;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent newint = getIntent();
+        address = newint.getStringExtra(DeviceList.EXTRA_ADDRESS);
+
+        setContentView(R.layout.activity_led_control);
+
+        btn1 = (Button) findViewById(R.id.button2);
+        btn2 = (Button) findViewById(R.id.button3);
+        btn3 = (Button) findViewById(R.id.button5);
+        btn4 = (Button) findViewById(R.id.button6);
+        btn5 = (Button) findViewById(R.id.button7);
+        btn6 = (Button) findViewById(R.id.button8);
+        btnDis = (Button) findViewById(R.id.button4);
+        lumn = (TextView) findViewById(R.id.textView2);
+        readData = (TextView) findViewById(R.id.readData);
+
+        new ConnectBT().execute();
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View v) {
+                sendSignal("1");
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View v) {
+                sendSignal("2");
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View v) {
+                sendSignal("3");
+            }
+        });
+
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View v) {
+                sendSignal("4");
+            }
+        });
+
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View v) {
+                sendSignal("5");
+            }
+        });
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View v) {
+                sendSignal("6");
+            }
+        });
+
+        btnDis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View v) {
+                Disconnect();
+            }
+        });
+    }
+
+    private void sendSignal ( String number ) {
+        if ( btSocket != null ) {
+            try {
+                btSocket.getOutputStream().write(number.toString().getBytes());
+            } catch (IOException e) {
+                msg("Error");
+            }
+        }
+    }
+
+    private void beginListenForData()
+    {
+        final byte delimiter = 10; //This is the ASCII code for a newline character
+
+        readBufferPosition = 0;
+        readBuffer = new byte[1024];
+
+        try
+        {
+            int bytesAvailable = btSocket.getInputStream().available();
+            if(bytesAvailable > 0)
+            {
+                byte[] packetBytes = new byte[bytesAvailable];
+               btSocket.getInputStream().read(packetBytes);
+
+                for(int i=0;i<bytesAvailable;i++)
+                {
+                    byte b = packetBytes[i];
+                    if(b == delimiter)
+                    {
+                        byte[] encodedBytes = new byte[readBufferPosition];
+                        System.arraycopy(readBuffer, 0, encodedBytes, 0, encodedBytes.length);
+                        final String data = new String(encodedBytes, "US-ASCII");
+                        readBufferPosition = 0;
+
+                        msg(data);
+                        readData.setText(data);
+                    }
+                    else
+                    {
+                        readBuffer[readBufferPosition++] = b;
+                    }
+                }
+            }
+        }
+        catch (IOException e)
+        {
+            msg("Error");
+        }
+    }
+
+    private void Disconnect () {
+        if ( btSocket!=null ) {
+            try {
+                btSocket.close();
+            } catch(IOException e) {
+                msg("Error");
+            }
+        }
+
+        finish();
+    }
+
+    private void msg (String s) {
+        Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
+    }
+
+    private class ConnectBT extends AsyncTask<Void, Void, Void> {
+        private boolean ConnectSuccess = true;
+
+        @Override
+        protected  void onPreExecute () {
+            progress = ProgressDialog.show(ledControl.this, "Connecting...", "Please Wait!!!");
+        }
+
+        @Override
+        protected Void doInBackground (Void... devices) {
+            try {
+                if ( btSocket==null || !isBtConnected ) {
+                    myBluetooth = BluetoothAdapter.getDefaultAdapter();
+                    BluetoothDevice dispositivo = myBluetooth.getRemoteDevice(address);
+                    btSocket = dispositivo.createInsecureRfcommSocketToServiceRecord(myUUID);
+                    BluetoothAdapter.getDefaultAdapter().cancelDiscovery();
+                    btSocket.connect();
+
+                    beginListenForData();
+                }
+            } catch (IOException e) {
+                ConnectSuccess = false;
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute (Void result) {
+            super.onPostExecute(result);
+
+            if (!ConnectSuccess) {
+                msg("Connection Failed. Is it a SPP Bluetooth? Try again.");
+                finish();
+            } else {
+                msg("Connected");
+                isBtConnected = true;
+            }
+
+            progress.dismiss();
+        }
+    }
+}
